91c17ee9027c35f74214f872f42bef2d2a629b3e
Lilaven <68438868+Lilaven@users.noreply.github.com>
2025-07-03 13:01:13 +0700

    Add files via upload

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var color_list = ["red","blue","green","yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
-let started = false
+let started = false;
 
 function nextSequence() {
     level++;
@@ -26,7 +26,7 @@ function nextSequence() {
     },1500);
     return level
     
-}
+};
 
 
 function btn_click(color) {
@@ -34,18 +34,18 @@ function btn_click(color) {
     $(id).fadeOut(50).fadeIn(50);
     playSound(color);
     animatePress(color);
-}
+};
 
 
 function playSound (sound_code) {
     let sound = new Audio("sounds/"+sound_code+".mp3");
     sound.play();
-}
+};
 
 function animatePress(currentColour) {
     $('#'+currentColour).addClass('pressed');
     setTimeout(()=>{$('#'+currentColour).removeClass('pressed');},100);
-}
+};
 
 function checkAnswer(currentLevel) {
     last = userClickedPattern.length;
@@ -59,38 +59,52 @@ function checkAnswer(currentLevel) {
         console.log('wrong');
         $('body').addClass('game-over');
         setTimeout(() => {$('body').removeClass('game-over');},200);
-        $('h1').text('Game Over, Press Any Key to Restart');
+        $('h1').html('<div class= "restart">Game Over <span><button class="btn-restart">PRESS TO RESTART</button></span></div>');
         $('h2').text('-----');
         playSound('wrong');
         startOver();
-    }
+        $(".btn-restart").on('click', function() {
+            console.log(started);
+            if (!started) {
+                started = true;
+                nextSequence(level);
+            };
+        });
+    };
     return level;
 
-}
+};
 
 function startOver() {
     gamePattern = [];
     let userClickedPattern = [];
     level = 0;
     started = false;
-}
+};
 
     
 $(document).on('keydown', function() {
     if (!started) {
-        $('h1').text("Press A Key to Start");
+        $('h1').html('<span><button class="start">PRESS TO START</button></span>');
         nextSequence(level);
         started = true;
-    }
-    
-    
-    
-})
+    } 
+});
+
+$(".start").on('click', function() {
+    if (!started) {
+        nextSequence(level);
+        started = true;
+    };
+});
 
 $('.btn').on('click', function() {
-    let userChosenColour = $(this).attr('id');
-    btn_click(userChosenColour);
-    userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
-    checkAnswer(level);
-})
+    if (started) {
+        let userChosenColour = $(this).attr('id');
+        btn_click(userChosenColour);
+        userClickedPattern.push(userChosenColour);
+        console.log(userClickedPattern);
+        checkAnswer(level);
+    };
+});
+
